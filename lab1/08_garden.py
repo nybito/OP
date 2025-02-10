ae25926dbb558c2ceae322b13983d788536ed9dd
nybito <82085949+nybito@users.noreply.github.com>
2025-02-10 12:37:15 +0500

    Update 08_garden.py

--- lab1/08_garden.py
+++ lab1/08_garden.py
@@ -11,15 +11,24 @@ meadow = ('клевер', 'одуванчик', 'ромашка', 'клевер'
 # garden_set =
 # meadow_set =
 # TODO здесь ваш код
-
+garden_set = set(garden)
+meadow_set = set(meadow)
 # выведите на консоль все виды цветов
 # TODO здесь ваш код
-
+all_flowers = garden_set.union(meadow_set)
+print(all_flowers)
 # выведите на консоль те, которые растут и там и там
 # TODO здесь ваш код
-
+print("\nЦветы, которые растут и в саду, и на лугу:")
+common_flowers = garden_set.intersection(meadow_set)
+print(common_flowers)
 # выведите на консоль те, которые растут в саду, но не растут на лугу
 # TODO здесь ваш код
-
+print("\nЦветы, которые растут в саду, но не растут на лугу:")
+only_garden_flowers = garden_set.difference(meadow_set)
+print(only_garden_flowers)
 # выведите на консоль те, которые растут на лугу, но не растут в саду
 # TODO здесь ваш код
+print("\nЦветы, которые растут на лугу, но не растут в саду:")
+only_meadow_flowers = meadow_set.difference(garden_set)
+print(only_meadow_flowers)
