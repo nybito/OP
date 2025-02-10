ca22f4e204987bbe4ac025e6b0300d384cbb17cb
nybito <82085949+nybito@users.noreply.github.com>
2025-02-10 12:32:21 +0500

    Update 00_distance.py

--- lab1/00_distance.py
+++ lab1/00_distance.py
@@ -15,7 +15,15 @@ sites = {
 distances = {}
 
 # TODO здесь заполнение словаря
+def calculate_distance(coord1, coord2):
+    return ((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2) ** 0.5
 
+for city1, coord1 in sites.items():
+    distances[city1] = {}
+    for city2, coord2 in sites.items():
+        if city1 != city2:  
+            distance = calculate_distance(coord1, coord2)
+            distances[city1][city2] = distance
 print(distances)
 
 
