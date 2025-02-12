e0f76463ed1a2561fb5c8914cae9f98c01659d72
nybito <82085949+nybito@users.noreply.github.com>
2025-02-13 03:44:19 +0500

    Update 3.py

--- lab2/3.py
+++ lab2/3.py
@@ -1,7 +1,8 @@
+import math
 z = 1
 for x in range(245690,245756):
     c = 0
-    for y in range(2,math.isqrt(x)+1):
+    for y in range(2,math.isqrt(x)):
         if x%y == 0:
             c+=1
     z+=1
