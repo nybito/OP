c814d4d5ba45ef28154b7fc58052753a4ef00fb6
nybito <82085949+nybito@users.noreply.github.com>
2025-02-19 17:30:31 +0500

    Update 1.py

--- laba3/1.py
+++ laba3/1.py
@@ -1,3 +1,4 @@
+#РЕКУРСИЯ
 def unpack_recursive(lst):
     result = []
     for item in lst:
@@ -12,3 +13,31 @@ def unpack_recursive(lst):
             # если это базовый тип, добавляем его в результат
             result.append(item)
     return result
+    
+# Не рекурсия
+def unpack_iterative(lst):
+    result = []
+    stack = lst.copy()  # создаем копию списка для обработки
+    while stack:
+        item = stack.pop()  # берём последний элемент из стека
+        if isinstance(item, (list, tuple, set)):
+            # если это структура, добавляем её элементы в стек
+            stack.extend(item)
+        elif isinstance(item, dict):
+            # если это словарь, добавляем ключи и значения
+            stack.extend(item.keys())
+            stack.extend(item.values())
+        else:
+            # если это базовый тип, добавляем его в результат
+            result.append(item)
+    return result[::-1]  # возвращаем результат в правильном порядке
+
+nested_list = [None, [1, ({2, 3}, {'foo': 'bar'})]]
+
+# Рекурсивный подход
+result_recursive = unpack_recursive(nested_list)
+print(result_recursive)  # Вывод: [None, 1, 2, 3, 'foo', 'bar']
+
+# Итеративный подход
+result_iterative = unpack_iterative(nested_list)
+print(result_iterative)  # Вывод: [None, 1, 2, 3, 'foo', 'bar']
